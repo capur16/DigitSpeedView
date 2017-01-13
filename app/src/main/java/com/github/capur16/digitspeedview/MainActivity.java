ce43025b09cb8f78a395767dc2a7d8b096c6d5da
Riccardo Capuani <riccardo.capuani@gmail.com>
2017-01-13 17:34:23 +0100

    Initial commit

---
+++ app/src/main/java/com/github/capur16/digitspeedview/MainActivity.java
@@ -0,0 +1,13 @@
+package com.github.capur16.digitspeedview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_digit_speed_view);
+    }
+}
---
+++ digitspeedviewlib/src/main/java/com/github/capur16/digitspeedview/DigitSpeedView.java
@@ -0,0 +1,8 @@
+package com.github.capur16.digitspeedview;
+
+/**
+ * Created by Riccardo on 13/01/2017.
+ */
+
+public class DigitSpeedView {
+}
